db98558b6739380400589acf9d1f225f4b8ade14
Temur Chichua <69351709+temurchichua@users.noreply.github.com>
2021-07-25 14:40:42 +0400

    publishing version

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -56,26 +56,6 @@ export default class HelloWorldSceneAR extends Component {
                             onFinish: this._onHekateFinished }}
                           type="OBJ"/>
 
-            <ViroSpotLight
-                innerAngle={5}
-                outerAngle={25}
-                direction={[0,-1,0]}
-                position={[0, 3, 1]}
-                color="#ffffff"
-                castsShadow={true}
-                shadowMapSize={2048}
-                shadowNearZ={2}
-                shadowFarZ={7}
-                shadowOpacity={.7} />
-
-            <ViroAmbientLight color="#FFFFFF" />
-
-            <ViroQuad
-                rotation={[-90, 0, 0]}
-                position={[0, -0.001, 0]}
-                width={2.5} height={2.5}
-                arShadowReceiver={true} />
-
           </ViroARImageMarker>
 
           <ViroARImageMarker target={"dolphin"} onAnchorFound={this._onDolphinFound} pauseUpdates={this.state.pauseUpdates}>
